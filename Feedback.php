deee2ab7e36fbe8c243e104200ee40803a8be003
michaela-ellis <93197122+michaela-ellis@users.noreply.github.com>
2021-10-27 14:56:13 +1300

    Update Feedback.php

--- Feedback.php
+++ Feedback.php
@@ -1,7 +1,23 @@
-<html>
-  <body>
 <?php
-echo readfile("Feedback.css");
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
 ?>
-</body>
-</html>  
+<?php
+	$email_from = 'michaela.ellis@raywhite.com';
+
+	$email_subject = "New Form submission";
+
+	$email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:
+?>
+<?php
+
+  $to = "michaela.ellis@yourwebsite.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+ ?>
